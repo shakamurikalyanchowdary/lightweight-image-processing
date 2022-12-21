a06874911533eee5bbd29a20f174ef99363423c9
shakamurikalyan <kalyan.s@keus.in>
2022-12-21 12:28:24 +0530

    changes

--- app/light-weight-imp-sdk/src/light-weight-imp-impl.ts
+++ app/light-weight-imp-sdk/src/light-weight-imp-impl.ts
@@ -1,17 +1,25 @@
 import express from 'express';
 import thread from 'threads';
+import { ILightWeightImp } from 'light-weight-imp-common'
 const app = express()
+import zx from 'zx';
 
-export class LWII {
-    static async start() {
-        app.get('/', function (req, res) {
-            console.log('running code')
-            res.send('Hi this is light weight image server');
-        })
-        app.get('/upload', function (req, res) {
-            
-        })
-        app.listen(6060)
+export class LWII extends ILightWeightImp {
+    async getlist(): Promise<[]> {
+        let data: any = await zx.$`cd ../storage && ls`
+        let fileList: [] = data['stdout'].split('\n')
+        return fileList
+    }
+    async upload(): Promise<string> {
+        throw new Error('Method not implemented.');
+    }
+    async delete(name: String): Promise<string> {
+        throw new Error('Method not implemented.');
+    }
+    async crop(name: string): Promise<string> {
+        throw new Error('Method not implemented.');
+    }
+    async resize(name: string): Promise<string> {
+        throw new Error('Method not implemented.');
     }
 }
-LWII.start();
